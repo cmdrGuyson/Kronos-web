0985c1228dd2c2f4e4d5d22caa3a08913bc18e1a
cmdrGuyson <gayangakuruppu@gmail.com>
2021-04-08 14:50:57 +0530

    Update SortLectureByTime.java

--- src/main/java/com/guyson/kronos/util/SortLectureByTime.java
+++ src/main/java/com/guyson/kronos/util/SortLectureByTime.java
@@ -2,11 +2,7 @@ package com.guyson.kronos.util;
 
 import com.guyson.kronos.domain.Lecture;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 
 //Sort given list of lectures by ascending order of time of day
